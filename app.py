51f41c26e8aacea8f15873dbf5f2e12b9077c4f5
Ely Alamillo <ely0013@gmail.com>
2018-11-29 01:31:43 -0600

    moved item logic to own file

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask_jwt_extended import (
 )
 
 from user import UserRegister
+from item import Item, ItemList
 
 
 app = Flask(__name__)
@@ -19,62 +20,6 @@ api = Api(app)
 app.config["JWT_SECRET_KEY"] = "secretkeyboyz"
 
 jwt = JWTManager(app)
-
-items = []
-
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        "price", type=float, required=True, help="Price cannot be blank."
-    )
-
-    @jwt_required
-    def get(self, name):
-        item = next(filter(lambda x: x["name"] == name, items), None)
-
-        return {"item": item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x["name"] == name, items), None):
-            # 400 bad request
-            return (
-                {
-                    "message": "An item with name '{}' already exists".format(
-                        name
-                    )
-                },
-                400,
-            )
-
-        data = Item.parser.parse_args()
-        price = data["price"]
-        item = {"name": name, "price": price}
-        items.append(item)
-        # 201 is for created
-        return item, 201
-
-    def delete(self, name):
-        # fixes scoping of item from local -> global
-
-        global items
-        items = list(filter(lambda x: x["name"] != name, items))
-        return {"message": "item deleted"}, 200
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-        item = next(filter(lambda x: x["name"] == name, items), None)
-        if item is None:
-            item = {"name": name, "price": data["price"]}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return {"items": items}
 
 
 class Auth(Resource):
